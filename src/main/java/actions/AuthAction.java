52cd5da8245c8c8541055086b90a19a4c04b8e3c
takako.tomimori <takatomato22@gmail.com>
2021-08-10 14:59:47 +0900

    ログアウトアクションの作成

--- src/main/java/actions/AuthAction.java
+++ src/main/java/actions/AuthAction.java
@@ -100,4 +100,20 @@ public class AuthAction extends ActionBase {
             forward(ForwardConst.FW_LOGIN);
         }
     }
+    /**
+     * ログアウト処理を行う
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void logout() throws ServletException,IOException{
+
+        //セッションからログイン従業員のパラメータを削除
+        removeSessionScope(AttributeConst.LOGIN_EMP);
+
+        //セッションにログアウト時のフラッシュメッセージを追加
+        putSessionScope(AttributeConst.FLUSH,MessageConst.I_LOGINED.getMessage());
+
+        //ログイン画面にリダイレクト
+        redirect(ForwardConst.ACT_AUTH,ForwardConst.CMD_SHOW_LOGIN);
+    }
 }
